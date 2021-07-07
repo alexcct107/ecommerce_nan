e7ea5f076d7175a56b9c82911603b7daa5104631
Ivan Carrasco <alexcct@hotmail.com>
2021-07-07 15:41:21 -0400

    Adding devise and models

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admins
+  root to: "home#index"
+
+  authenticate :admin do
+    resources :products
+    resources :categories
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ test/models/order_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class OrderTest < ActiveSupport::TestCase
+   test "creates a random number on create" do
+    user = User.create(email: "user@example.com", password: "12345678")
+    order = Order.create(user_id: user.id)
+    assert !order.number.nil?
+  #   assert true
+  end
+
+  test 'number must be unique' do
+    user = User.create(email: "user@example.com", password: "12345678")
+    order = Order.create(user_id: user.id)
+    duplicated_order = order.dup
+    assert_not duplicated_order.valid?
+   end
+end
